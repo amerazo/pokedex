6025a7a9e2fbfc061048701b86146c6e1664f0c3
secretivo <secretivo@secretivos-Air.lan>
2023-04-30 20:54:17 -0400

    set up routes

--- server.js
+++ server.js
@@ -3,15 +3,49 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-//Root Route
+// Middleware
+app.set('view engine', 'ejs');
+//View Engine Template Engine
+app.use(express.static('public'));
+//Tells app to look for static files in the public directory. 
+app.use(express.urlencoded({ extended:false }));
+//Adds to the req.body object. This middleware is required to access the form data submitted in a POST request.
+app.use(express.json());
+//https://expressjs.com/en/resources/middleware/method-override.html#:~:text=Lets%20you%20use%20HTTP%20verbs,client%20doesn't%20support%20it.
+app.use(methodOverride("_method")); 
+
+
+//Root Route: GET
 app.get('/', (req, res) => {
     res.redirect('/pokemon');
   });
 
-//Index Route
+//Index Route: GET
   app.get('/pokemon', (req, res) => {
     res.render('index.ejs');
   });
+
+//New Route: GET
+  app.get("/pokemon/new", (req, res)=>{
+    res.render('new.ejs');
+})
+
+// Create route: POST
+app.post('/pokemon', (req, res) => {
+  });
+  
+// Edit route: GET
+app.get('/pokemon/:id/edit', (req, res) => {
+    //('edit.ejs')
+  });
+  
+// Update route: PUT
+app.put('/pokemon/:id', (req, res) => {
+  });
+  
+// Destroy route: DELETE
+app.delete('/pokemon/:id', (req, res) => {
+});
   
 
 //PORT
